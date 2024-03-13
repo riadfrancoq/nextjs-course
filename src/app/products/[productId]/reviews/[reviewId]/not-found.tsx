8c0799a3a274daf066f5206e6f97ddd4cc293d63
riadfrancoq <campusricardo2023@gmail.com>
2024-03-13 13:43:33 -0500

    feat: :sparkles: Not Found Page

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,6 @@
+
+export default function ReviewNotFound() {
+    return  (
+        <h1>Review Not Found</h1>
+    );
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,12 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({params}: {
     params: { productId: string, reviewId: string }
 }) {
+    if (parseInt(params.reviewId) > 1000 || Number.isNaN(Number(params.reviewId)) ) {
+        notFound();
+    }
+
     return  (
         <h1>Details About the review {params.reviewId} of product {params.productId} </h1>
     );

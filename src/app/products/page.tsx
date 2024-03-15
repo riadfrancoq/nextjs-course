423d7f0025f92723302ababf183558a206cb72c9
riadfrancoq <campusricardo2023@gmail.com>
2024-03-14 22:55:07 -0500

    feat: :sparkles: Link component navigation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 export default function ProductList() {
+    const productId = 100;
     return  (
         <>
-        <h1>Product List</h1>
-        <h1>Product 1</h1>
-        <h1>Product 2</h1>
-        <h1>Product 3</h1>
-        
+        <h2> <Link href="/"> Home</Link> </h2>
+        <h2> Product List </h2>
+        <h2> <Link href="products/1">Product 1</Link> </h2>
+        <h2> <Link href="products/2">Product 2</Link> </h2>
+        <h2> <Link href="products/3" replace>Product 3</Link> </h2>
+        <h2> <Link href={`products/${productId}`}> Product {productId}</Link></h2>
         </>  
     );
 };

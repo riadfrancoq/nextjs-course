8154d82f5dc7f1fe26a9bc3151962dc17f99ffee
riadfrancoq <campusricardo2023@gmail.com>
2024-03-14 22:08:52 -0500

    feat: :sparkles: title metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog" // The template has been ignored
+    }
+
+}
 
 export default function Blog() {
     return <h1> Hello Blog</h1>

9270e6df92c31e73f686354e61cd3370ca43fc78
riadfrancoq <campusricardo2023@gmail.com>
2024-03-14 23:29:18 -0500

    feat: :sparkles: Active Links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,8 +8,10 @@ export default function ProductList() {
         <h2> Product List </h2>
         <h2> <Link href="products/1">Product 1</Link> </h2>
         <h2> <Link href="products/2">Product 2</Link> </h2>
-        <h2> <Link href="products/3" replace>Product 3</Link> </h2>
+        <h2> <Link href="products/3" replace>Product 3</Link> </h2> 
         <h2> <Link href={`products/${productId}`}> Product {productId}</Link></h2>
         </>  
     );
+
+    // replace -  Replace the current history state instead of adding a new url into 
 };

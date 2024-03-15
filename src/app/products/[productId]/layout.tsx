0dc756216b2947afc2668fa61010d40304b1435a
riadfrancoq <campusricardo2023@gmail.com>
2024-03-15 01:00:44 -0500

    feat: :sparkles: Handling Errors in Layouts

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,8 +1,18 @@
+function getRandomInt(count: number){
+  return Math.floor(Math.random() * count);
+};
+
   export default function ProductDetailsLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
+    const random : number = getRandomInt(2);
+
+    if (random === 1) {
+        throw new Error("Error loading product")
+    }
+
     return (
         <>
           {children}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -8,11 +8,11 @@ function getRandomInt(count: number){
 export default function ReviewDetails({params}: {
     params: { productId: string, reviewId: string }
 }) {
-    const random : number = getRandomInt(2);
+    //const random : number = getRandomInt(2);
 
-    if (random === 1) {
-        throw new Error("Error loading review")
-    }
+    //if (random === 1) {
+        //throw new Error("Error loading review")
+    //}
     if (parseInt(params.reviewId) > 1000 || Number.isNaN(Number(params.reviewId)) ) {
         notFound();
     }

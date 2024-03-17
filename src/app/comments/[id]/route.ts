1541866745ca69e0e2734bbc81f102d144ec98a3
riadfrancoq <campusricardo2023@gmail.com>
2024-03-17 16:26:40 -0500

    feat: :sparkles: Handling DELETE Request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -18,3 +18,15 @@ export async function  PATCH(req: Request, context: {
     return Response.json(comments[index]);
 
 }
+
+
+export async function DELETE(_req: Request, context: {
+    params: {
+        id: string
+    }
+}) {
+    const index = comments.findIndex(comment => comment.id === parseInt(context.params.id));
+    const deletedComment = comments[index]
+    comments.splice(index, 1);
+    return Response.json(deletedComment);
+}

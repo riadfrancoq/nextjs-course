eebab384a266e72abf90738d4d5946c65fe219fd
riadfrancoq <campusricardo2023@gmail.com>
2024-03-17 16:06:24 -0500

    feat: :sparkles: Handling POST Request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,3 +3,17 @@ import { comments } from "./data";
 export async function GET() {
     return Response.json(comments);
 }
+
+export async function POST(req: Request) {
+    const comment = await req.json();
+    const newComment = {
+        id: comments.length + 1,
+        text: comment.text
+    };
+    return new Response(JSON.stringify(newComment), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        status: 201
+    });
+}

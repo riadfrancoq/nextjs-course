bced9f6977929aab891f019dd0561a37d0053c48
riadfrancoq <campusricardo2023@gmail.com>
2024-03-17 17:08:56 -0500

    feat: :sparkles: Headers in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET() {
-    return new Response('Profile data');
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(req: NextRequest) {
+    const requestHeaders = new Headers(req.headers);
+    const headersList = headers();
+    console.log(requestHeaders.get('Authorization')); // Both can be used 
+    console.log(headersList.get('Authorization'));
+    return new Response('<h1>Profile data</h1>', {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    });
 }
+

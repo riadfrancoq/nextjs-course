ae0ea0fae835c1516a1be9dce991f20fdbd77cac
riadfrancoq <campusricardo2023@gmail.com>
2024-03-17 16:00:13 -0500

    feat: :sparkles: Handling GET Request

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id: 2,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+        id: 3,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+]

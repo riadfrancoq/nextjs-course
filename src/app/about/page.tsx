7eb6ca9431014e619d84fb4ce81b21c5356d8419
riadfrancoq <campusricardo2023@gmail.com>
2024-03-18 02:04:21 -0500

    feat: :sparkles: Dynamic Rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
+import { cookies } from "next/headers";
+
 export default function About() {
+    const cookieStore = cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
+    
     console.log("About Server Component")
     return <h1>About Page{new Date().toLocaleString()}</h1>;
     
